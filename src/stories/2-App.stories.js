3eef30c7ef210e1daf133e7dc44cb41a6ea73d0b
Truong Nguyen <nhattruongniit@yahoo.com.vn>
2020-06-12 22:54:03 +0700

    add storybook knobs

--- src/stories/2-App.stories.js
+++ src/stories/2-App.stories.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { action } from '@storybook/addon-actions';
-import { Button } from '@storybook/react/demo';
+import { withKnobs, text, number } from "@storybook/addon-knobs";
 
 import App from '../App';
 
 export default {
   title: 'App',
+  decorators: [withKnobs]
 };
 
 export const AppDefault = () => <App/>
 
 export const AppWithTitle = () => <App title="tony"/>
+
+export const asDynamicVariables = () => {
+  const name = text("Name", "James");
+  const age = number("Age", 35);
+  const title = text("title", "tony");
+  const content = `I am ${name} and I'm ${age} years old.`;
+ 
+  return (
+    <>
+      <App title={title} />
+      {content}
+    </>
+  )
+};
