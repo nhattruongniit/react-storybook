01867dc3823ffd316bac4375e0dbf101228ed7ea
Truong Nguyen <nhattruongniit@yahoo.com.vn>
2020-06-12 22:59:02 +0700

    add storybook knobs

--- src/stories/2-App.stories.js
+++ src/stories/2-App.stories.js
@@ -25,3 +25,18 @@ export const asDynamicVariables = () => {
     </>
   )
 };
+
+export const inGroups = () => {
+  const personalGroupId = 'personal info';
+  const generalGroupId = 'general info';
+ 
+  const name = text("Name", "James", personalGroupId);
+  const age = number("Age", 35, {min: 0, max: 99}, personalGroupId);
+  const message = text("Hello!", 35, generalGroupId);
+  const content = `
+    I am ${name} and I'm ${age} years old.
+    ${message}
+  `;
+ 
+  return <div>{content}</div>;
+};
